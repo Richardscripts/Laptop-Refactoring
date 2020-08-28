b398a9bf428cefed9ee11d1e9786ba83d15e3243
Richard Carmen Scott <RichardCarmenScott@gmail.com>
2020-08-28 15:10:30 -0400

    Initial dist subtree commit

---
+++ precache-manifest.2f741111e0a582be86008a918aced36b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf0938e20a8d654ffc8b855f363faf9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5cccc1e37dc091f9a22a",
+    "url": "/static/css/main.72a41afd.chunk.css"
+  },
+  {
+    "revision": "91be17f00bf027269ee1",
+    "url": "/static/js/2.a3f18fbf.chunk.js"
+  },
+  {
+    "revision": "5cccc1e37dc091f9a22a",
+    "url": "/static/js/main.4fd5683e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
